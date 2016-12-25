8f76c11e652153ee1709fb83793a3c092a658137
ratuljain1991 <lol@Ratul.local>
2016-12-26 01:18:43 +0530

    Changed hosts file

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,3 +1,3 @@
  #!/bin/bash
 export REMOTE_DEPLOYER_PASSWORD='scooty2310';
-ansible-playbook ./prod/deploy.yml --private-key=./ssh_keys/prod_key -u deployer -i ./prod/hosts;
+ansible-playbook ./prod/deploy.yml -vvv --private-key=./ssh_keys/prod_key -u deployer -i ./prod/hosts;
