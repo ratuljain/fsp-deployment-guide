4f63e9aaebeeb0dd46c7bcc3c82d9b5262285a20
ratuljain1991 <lol@Ratul.local>
2016-12-26 02:25:15 +0530

    Changed default directory for ssh keys to match the jenkins server

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,3 +1,4 @@
  #!/bin/bash
-export REMOTE_DEPLOYER_PASSWORD='scooty2310';
-ansible-playbook ./prod/deploy.yml -vvv --private-key=./ssh_keys/prod_key -u deployer -i ./prod/hosts;
+ export REMOTE_DEPLOYER_PASSWORD='scooty2310';
+ #ansible-playbook ./prod/deploy.yml -vvv --private-key=./ssh_keys/prod_key -u deployer -i ./prod/hosts;
+ ansible-playbook /var/lib/jenkins/workspace/fsp-deployment-guide/prod/deploy.yml -vvv --private-key=/var/lib/jenkins/workspace/fsp-deployment-guide/ssh_keys/prod_key -u deployer -i /var/lib/jenkins/workspace/fsp-deployment-guide/prod/hosts;
